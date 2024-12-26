1596f0a4b48f6f1bd54c94e3eb8200524356e6fe
thicco-catto <cristobal.martin01@gmail.com>
2024-12-26 16:36:52 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use dioxus::prelude::*;
+
+use components::Hero;
+
+mod components;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    // Build cool things ✌️
+
+    rsx! {
+        // Global app resources
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+
+
+        Hero {}
+
+    }
+}
